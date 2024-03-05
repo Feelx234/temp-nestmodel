06487a8ab83996c9aa04f38e007a7a02e9a303ad
Feelx234 <felix.stamm@rwth-aachen.de>
2024-03-05 15:35:53 +0100

    implemented O(E+T) datastructure to obtain colors for temporal graph

--- src/tnestmodel/temp_wl.py
+++ src/tnestmodel/temp_wl.py
@@ -1,3 +1,4 @@
+from collections import deque
 import numpy as np
 from numba import njit
 
@@ -32,7 +33,7 @@ def convert_edges_to_active_edges(E, num_active_per_node, num_active_nodes, num_
     #last_active_node[0]=-1
     #last_active_node[1:] = np.cumsum(num_active)[:-1]-1
 
-    E_out = np.empty((len(E), 2), dtype = np.int64)
+    E_active = np.empty((len(E), 2), dtype = np.int64)
     times_for_active = np.empty(num_active_nodes, dtype=np.int64)
 
     for i, (u,v,t) in enumerate(E):
@@ -47,9 +48,9 @@ def convert_edges_to_active_edges(E, num_active_per_node, num_active_nodes, num_
             last_active_node[v]+=1
             times_for_active[last_active_node[v]]=t
         new_v = last_active_node[v]
-        E_out[i,0] = new_u
-        E_out[i,1] = new_v
-    return E_out, times_for_active
+        E_active[i,0] = new_u
+        E_active[i,1] = new_v
+    return E_active, times_for_active
 
 
 MAX_HASH_BITS=64
@@ -74,6 +75,23 @@ def compute_d_rounds(E : np.ndarray, num_nodes : int, d : int, h : int=-1, seed 
     h: absolute horizon of temporal nodes, i.e. an edge at time t can see nodes up to and including t+h; h<0 means infinite horizon
     seed: seed used to initialize pseudo random number generator generating used hash values
     """
+    colors_per_round, _, _, num_active_per_node, E, _, times_for_active = _compute_d_rounds(
+        E=E,
+        num_nodes=num_nodes,
+        d = d,
+        h=h,
+        seed=seed,
+    )
+    return colors_per_round, repeat_active_nodes(num_active_per_node), times_for_active
+
+def _compute_d_rounds(E : np.ndarray, num_nodes : int, d : int, h : int=-1, seed : int=0):
+    """Computes temporal wl of temporal edges E
+    E : temporal edges, size = (num_edges, 3); u,v,t = E[0,:] represents a directed edge u->v at time t
+    num_nodes: the number of non-temporal nodes
+    d: maximum number of rounds to do the wl-algorithm; d<0 means compute until convergence
+    h: absolute horizon of temporal nodes, i.e. an edge at time t can see nodes up to and including t+h; h<0 means infinite horizon
+    seed: seed used to initialize pseudo random number generator generating used hash values
+    """
     assert d != 0
     order_in_time = np.argsort(E[:, 2])
     smallest_time = E[order_in_time[0],2]
@@ -84,15 +102,15 @@ def compute_d_rounds(E : np.ndarray, num_nodes : int, d : int, h : int=-1, seed 
     total_active_nodes = num_active_per_node.sum()
     max_num_active = num_active_per_node.max() # maximum degree of a node, necessary to limit hash sizes
 
-    E_out, times_for_active = convert_edges_to_active_edges(E, num_active_per_node, total_active_nodes, num_nodes)
+    E_active, times_for_active = convert_edges_to_active_edges(E, num_active_per_node, total_active_nodes, num_nodes)
 
 
     hashes = get_random_hashes(2 * total_active_nodes, max_sum_length = max_num_active+1, seed=seed)
     hashes[total_active_nodes] = 0  # assign degree zero the zero hash
 
     colors = np.zeros(total_active_nodes, dtype=np.uint64)
-    out_colors = [colors]
-    list_cumsum_hashes = [colors]
+    colors_per_round = [colors]
+    cumsum_hashes_per_round = []
 
     if d > 0:
         max_num_iterations = d
@@ -100,20 +118,30 @@ def compute_d_rounds(E : np.ndarray, num_nodes : int, d : int, h : int=-1, seed 
         max_num_iterations  = total_active_nodes
     num_prev_colors = len(np.unique(colors))
     for _ in range(max_num_iterations):
-        new_colors, cumsum_hashes = one_round(hashes, out_colors[-1], num_prev_colors, E_out, num_active_per_node, total_active_nodes, times_for_active, h)
+        new_colors, cumsum_hashes = one_round(hashes, colors_per_round[-1], num_prev_colors, E_active, num_active_per_node, total_active_nodes, times_for_active, h)
 
         max_colors = new_colors.max()+1
         if max_colors==num_prev_colors: # stable colors reached, terminate
             break
         num_prev_colors = max_colors
-        out_colors.append(new_colors)
-        list_cumsum_hashes.append(cumsum_hashes)
+        colors_per_round.append(new_colors)
+        cumsum_hashes_per_round.append(cumsum_hashes)
 
-    return out_colors, repeat_active_nodes(num_active_per_node), times_for_active
+
+    return colors_per_round, cumsum_hashes_per_round, hashes, num_active_per_node, E, E_active, times_for_active
+
+
+
+
 
 
 
 class TemporalColorsStruct:
+    """A structure able to provide the current color of all nodes
+
+    Runtime:
+        When iterating increasing in time, this provides the current colors of all nodes in O(E+T)
+    """
     def __init__(self, colors_per_round, cumsum_hashes_per_round, hashes, num_active_per_node, E, E_active, times_for_active):
         self.colors_per_round=colors_per_round
         self.cumsum_hashes_per_round=cumsum_hashes_per_round
@@ -125,84 +153,138 @@ class TemporalColorsStruct:
         self.times_for_active = times_for_active
 
         self.colors_for_active = None
-        self.current_colors = None
-        self.cumsum_hashes = None
+
         self.d = None
+        self.h = None
         self.t = None
         self.base_active_node = cumsum_from_zero(self.num_active_per_node, full=True)
-        self.left_active_node = cumsum_from_zero(self.num_active_per_node, full=False)
-        self.right_active_node = self.left_active_node.copy()
         self.num_nodes = len(num_active_per_node)
+
+        self.prev_colors = None
+        self.cumsum_hashes = None
+
+        self.current_colors = np.zeros(self.num_nodes, dtype=np.int64)
+        self.current_hashes = np.zeros(self.num_nodes, dtype=np.uint64)
         self.left_e_index = None
         self.right_e_index = None
-        self.hash_dict = None
+        self.hash_dict = {}
+        self.color_count_arr = np.zeros(self.num_nodes, dtype=np.int64)
+        self.mode="global"
+        self.color_count_arr = np.zeros(self.num_nodes, dtype=np.int64)
+        self.colorqueue = None
 
-    def prepare(self, d, h):
+    def reset_colors(self, d, h, mode=None):
+        """Resets colors and allows to restart the coloring process"""
         self.d = d
+        assert h > 0
         self.h = h
         self.t = -1
         if d > 0:
-            self.prev_colors = self.colors_per_round[d-1]
-            self.cumsum_hashes = self.cumsum_hashes_per_round[d-1]
-        
-        self.current_colors = np.zeros(self.num_nodes, dtype=np.int64)
+            self.prev_colors = self.colors_per_round[d-1] # currently unused only needed if initial colors is supported
+            self.cumsum_hashes = np.zeros_like(self.cumsum_hashes_per_round[d-1])
+        if not mode is None:
+            self.mode=mode
+        self.current_colors[:]=0
+        self.current_hashes[:]=0
         self.left_e_index = 0
         self.right_e_index = 0
-        self.hash_dict = {}
+        self.hash_dict.clear()
+        self.color_count_arr[:]=0
+        #if self.mode != "global":
+        self.colorqueue = deque(range(1,self.num_nodes+1)) # color zero reserved for degree 0 nodes
+
+
 
     def advance_time(self, t):
-        assert self.t < t, "Can only move forward in time, if you want to go to an earlier time use .prepare again"
+        """Computes the colors of all nodes for time t
 
+        This function runs in O(E)
+
+        It returns the name of nodes whose color changed during this update
+        """
+
+        assert self.t < t, "Can only move forward in time, if you want to go to an earlier time use .reset_colors again"
+        if self.d == 0:
+            return set()
         self.t = t
-
         changed_nodes = set()
 
         # process edges no longer visible
-        i = self.left_e_index
-        while i < len(self.E):
-            #(u_act, v_act, t_act) = self.E_active[i,:]
-            (u, v, t_E) = self.E[i,:]
+        #  we find all nodes that loose an edge by increasing the time
+        #  for these we increase the left active node counter which
+        #  will subsequently affect that particular nodes hash
+        while self.left_e_index < len(self.E):
+            (u_act, _) = self.E_active[self.left_e_index,:]
+            (_, v, t_e) = self.E[self.left_e_index,:]
+
             #assert t_act==t_E
-            if t_E >= t:
+            if t_e >= t:
                 # we arrived at edges that can be potentially seen
                 break
-            self.left_active_node[v]+=1
+
+            #self.left_active_node[v]+=1
+            hash_to_add = self.hashes[self.prev_colors[u_act]]
+            self.cumsum_hashes[v]-=hash_to_add
             changed_nodes.add(v)
-            i+=1
-        self.left_e_index = i
+            self.left_e_index+=1
 
         # process newly visible edges
-        i = self.right_e_index
-        while i < len(self.E):
-            #(u_act, v_act, t_act) = self.E_active[i,:]
-            (u, v, t_E) = self.E[i,:]
-            
-            if t_E > t + self.h:
+        #  by advancing the time, we add newly visible edges
+        while self.right_e_index < len(self.E):
+            (u_act, _) = self.E_active[self.right_e_index,:]
+            (_, v, t_e) = self.E[self.right_e_index,:]
+
+            if t_e > t + self.h:
                 # we no longer see this edge or any future edges, break
                 break
-            self.right_active_node[v]+=1
+            #self.right_active_node[v]+=1
+            hash_to_add = self.hashes[self.prev_colors[u_act]]
+            self.cumsum_hashes[v]+=hash_to_add
             changed_nodes.add(v)
-            i+=1
-        self.right_e_index = i
+            self.right_e_index+=1
 
         for v in changed_nodes:
-            left = self.right_active_node[v]
-            right = self.right_active_node[v]
-            if  left == right:
-                self.current_colors[v] = 0 # this node does not see any degree
-            else:
-                if right == self.base_active_node[v+1]:
-                    the_hash = self.cumsum_hashes[left] # right is zero hash
-                else:
-                    the_hash = self.cumsum_hashes[left] - self.cumsum_hashes[right]
-                #hash += self.prev_color[v]
+            if  self.cumsum_hashes[v]==0:
+                self.current_colors[v] = 0 # this node does not see any edges
+                continue
+            the_hash = self.cumsum_hashes[v]
+
+            if self.mode=="global":
+                # uses globally valid colors, size of hash dict needs to grow
                 if the_hash in self.hash_dict:
                     self.current_colors[v] = self.hash_dict[the_hash]
-
                 else:
-                    new_color = len(self.hash_dict)
+                    new_color = len(self.hash_dict)+1 # color zero reserved for degree zero nodes
+                    self.current_colors[v] = new_color
                     self.hash_dict[the_hash]= new_color
-                    
+            elif self.mode == "local":
+                # in this mode we only keep only those hashes that are relevant in this slice
+                # the purpose of this is to keep the memory requirement to O(V+E)
+
+                # decrease count of previous color
+                #  if this color has no remaining uses, free this color for future uses
+                last_color = self.current_colors[v]
+                self.color_count_arr[last_color] -=1
+                if self.color_count_arr[last_color] == 0:
+                    self.colorqueue.appendleft(last_color)
+                    del self.hash_dict[self.current_hashes[v]]
+                self.current_hashes[v] = the_hash
+
+
+                if the_hash in self.hash_dict:
+                    # use the color associated with this hash
+                    self.current_colors[v] = self.hash_dict[the_hash]
+                else:
+                    # use a nre colos and make it available to other nodes by storing
+                    #  it in the hash_dict
+                    new_color = self.colorqueue.popleft()
+                    self.current_colors[v] = new_color
+                    self.hash_dict[the_hash]= new_color
+                # make sure the number of nodes with this color is correct
+                self.color_count_arr[self.current_colors[v]] +=1
+            else:
+                assert False, "This should not be reached"
+        return changed_nodes
 
 
 
@@ -236,14 +318,14 @@ def one_round(hashes : np.ndarray, colors : np.ndarray, num_colors : int, E_acti
             for i in range(n+d-2, n-1, -1):
                 simple_hashes[i]+=simple_hashes[i+1]
         n+=d
-    
+
     cumsum_hashes = simple_hashes
     cumsum_hashes_out = cumsum_hashes.copy()
     if h == -1:
         agg_hashes = cumsum_hashes
     else:
         agg_hashes = adjust_hashes_for_finite_horizon(cumsum_hashes, num_active_per_node, times, h)
-
+    #print(agg_hashes)
     ## if external colors are available
     #if num_colors > 1:
     #    for i, c in enumerate(colors):
--- tests/test_temporal_wl.py
+++ tests/test_temporal_wl.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_array_equal
 #from tnestmodel.temp_fast_graph import get_rolling_max_degree
 
 
-from tnestmodel.temp_wl import compute_d_rounds
+from tnestmodel.temp_wl import compute_d_rounds, _compute_d_rounds, TemporalColorsStruct
 from tnestmodel.wl_utils import assert_partitions_equivalent
 
 E1 = np.array([(1,0,0), (1,2,1), (3,1,2), (2,1,2)], dtype=np.int64)
@@ -87,6 +87,117 @@ class TestTemporalWL(unittest.TestCase):
         assert_partitions_equivalent(colors[3], [3, 1, 2, 1, 4, 5, 4, 4, 0, 0])
         assert_array_equal(nodes, nodes2)
         assert_array_equal(times, times2)
+
+
+
+
+class TestTemporalStruct(unittest.TestCase):
+    def check_partitions_agree(self, s, times, solutions, check_global=True):
+        global_partitions = []
+        for t, sol in zip(times, solutions):
+            s.advance_time(t)
+            if check_global:
+                global_partitions.append(s.current_colors.copy())
+            assert_partitions_equivalent(s.current_colors, sol)
+        if check_global:
+            sols = np.array(solutions, dtype=np.int64).ravel()
+            global_array = np.array(global_partitions, dtype=np.int64).ravel()
+            assert_partitions_equivalent(sols, global_array)
+
+
+    def test_global_fill_h1_d1(self):
+        h=1
+        s = TemporalColorsStruct(*_compute_d_rounds(E2, 4, d=-1, h=h))
+        s.reset_colors(d=1, h = h)
+        solutions = [
+            [1,1,0,0],
+            [0,1,1,0],
+            [0,1,1,0],
+            [0,1,1,0],
+            [0,0,1,0]
+        ]
+        self.check_partitions_agree(s, range(5), solutions)
+
+
+    def test_global_fill_h2_d1(self):
+        h=2
+        s = TemporalColorsStruct(*_compute_d_rounds(E2, 4, d=-1, h=h))
+        s.reset_colors(d=1, h = h)
+        solutions = [
+            [1,1,1,0],
+            [0,2,1,0],
+            [0,1,2,0],
+            [0,1,1,0],
+            [0,0,1,0]
+        ]
+        self.check_partitions_agree(s, range(5), solutions)
+
+
+    def test_global_fill_h2_d2(self):
+        h=2
+        s = TemporalColorsStruct(*_compute_d_rounds(E2, 4, d=-1, h=h))
+        s.reset_colors(d=2, h = h)
+        solutions = [
+            [1,1,2,0],
+            [0,3,2,0],
+            [0,1,4,0],
+            [0,1,2,0],
+            [0,0,2,0]
+        ]
+        self.check_partitions_agree(s, range(5), solutions)
+        s.reset_colors(d=2, h=h)
+        self.check_partitions_agree(s, range(5), solutions)
+        s.reset_colors(d=2, h=h)
+        s.advance_time(0)
+        # #assert_partitions_equivalent(s.current_colors, [1,1,1,0])
+        # print("<<<<<", s.current_colors)
+
+        # s.advance_time(1)
+        # #assert_partitions_equivalent(s.current_colors, [0,2,1,0])
+        # print("<<<<<", s.current_colors)
+
+        # s.advance_time(2)
+        # #assert_partitions_equivalent(s.current_colors, [0,1,2,0])
+        # print("<<<<<", s.current_colors)
+
+        # s.advance_time(3)
+        # #assert_partitions_equivalent(s.current_colors, [0,1,1,0])
+        # print("<<<<<", s.current_colors)
+
+        # s.advance_time(4)
+        # #assert_partitions_equivalent(s.current_colors, [0,0,1,0])
+        # print("<<<<<", s.current_colors)
+
+
+    def test_global_fill_h1_d1_local(self):
+        h=1
+        s = TemporalColorsStruct(*_compute_d_rounds(E2, 4, d=-1, h=h))
+        s.reset_colors(d=1, h = h, mode="local")
+        solutions = [
+            [1,1,0,0],
+            [0,1,1,0],
+            [0,1,1,0],
+            [0,1,1,0],
+            [0,0,1,0]
+        ]
+        self.check_partitions_agree(s, range(5), solutions, check_global=False)
+
+
+    def test_global_fill_h2_d2_local(self):
+        h=2
+        s = TemporalColorsStruct(*_compute_d_rounds(E2, 4, d=-1, h=h))
+        s.reset_colors(d=2, h = h, mode="local")
+        solutions = [
+            [1,1,2,0],
+            [0,3,2,0],
+            [0,1,4,0],
+            [0,1,2,0],
+            [0,0,2,0]
+        ]
+        self.check_partitions_agree(s, range(5), solutions, check_global=False)
+        s.reset_colors(d=2, h=h)
+        self.check_partitions_agree(s, range(5), solutions, check_global=False)
+
 
 if __name__ == '__main__':
     unittest.main()
